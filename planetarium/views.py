af6f26e84a827b175a66b05b4f1b7ffa0be3dc31
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-12-11 18:08:10 +0200

    upadate view, urls

---
+++ planetarium/views.py
@@ -0,0 +1,102 @@
+from django.shortcuts import render
+from datetime import datetime
+
+from django.db.models import F, Count
+from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
+
+from planetarium.models import AstronomyShow, ShowTheme, PlanetariumDome, ShowSession, Reservation, Ticket
+from planetarium.serializers.astronomy_show import AstronomyShowSerializer, AstronomyShowListSerializer, \
+    AstronomyShowDetailSerializer
+from planetarium.serializers.planetarium_dome import PlanetariumDomeSerializer
+
+from planetarium.serializers.show_session import (
+    ShowSessionListSerializer,
+    ShowSessionDetailSerializer,
+    ShowSessionSerializer,
+)
+from planetarium.serializers.show_theme import ShowThemeSerializer, ShowThemeListSerializer, ShowThemeDetailSerializer
+
+
+class ShowSessionViewSet(viewsets.ModelViewSet):
+    queryset = ShowSession.objects.all().select_related(
+        "astronomy_show", "planetarium_dome"
+    )
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        date = self.request.query_params.get("date")
+        show_id = self.request.query_params.get("show_id")
+        show_name = self.request.query_params.get("show_name")
+
+        if date:
+            date = datetime.strptime(date, "%Y-%m-%d").date()
+            queryset = queryset.filter(show_time__date=date)
+
+        if show_id:
+            queryset = queryset.filter(astronomy_show_id=show_id)
+
+        if show_name:
+            queryset = queryset.filter(astronomy_show__title__icontains=show_name)
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ShowSessionListSerializer
+
+        if self.action == "retrieve":
+            return ShowSessionDetailSerializer
+
+        return ShowSessionSerializer
+
+
+class PlanetariumDomeViewSet(viewsets.ModelViewSet):
+    queryset = PlanetariumDome.objects.all()
+    serializer_class = PlanetariumDomeSerializer
+
+
+class AstronomyShowViewSet(viewsets.ModelViewSet):
+    queryset = AstronomyShow.objects.all().prefetch_related("show_themes")
+    serializer_class = AstronomyShowSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return AstronomyShowListSerializer
+
+        if self.action == "retrieve":
+            return AstronomyShowDetailSerializer
+
+        return AstronomyShowSerializer
+
+
+class ShowThemeViewSet(viewsets.ModelViewSet):
+    queryset = ShowTheme.objects.all()
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        show_id = self.request.query_params.get("show_id")
+        show_name = self.request.query_params.get("show_name")
+
+        if show_id:
+            queryset = queryset.filter(astronomy_show_id=show_id)
+
+        if show_name:
+            queryset = queryset.filter(astronomy_show__title__icontains=show_name)
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ShowThemeListSerializer
+
+        if self.action == "retrieve":
+            return ShowThemeDetailSerializer
+
+        return ShowThemeSerializer
